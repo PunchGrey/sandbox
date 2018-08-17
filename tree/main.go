b4262284d7854278b64be73a07ea0846ebf9fe11
PunchGrey <GalSergGrey@yandex.ru>
2018-08-17 11:19:08 +0300

    Доработал функцию dirTree

--- tree/main.go
+++ tree/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -40,18 +42,28 @@ func main() {
 		//	fmt.Println(folder)
 		//printfListFolder(folder, "")
 		fmt.Print(printStringListFolder(folder, ""))*/
-	fmt.Print(dirTree(".."))
-}
+	path := flag.String("path", "..", "the root leaf for tree")
+	fileShow := flag.Bool("f", false, "show files")
+	flag.Parse()
 
-func dirTree(myPath string) string {
-	folder, err := getListFolder(myPath, nil)
+	out := new(bytes.Buffer)
+	err := dirTree(out, *path, *fileShow)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
-	return printStringListFolder(folder, "")
+	fmt.Print(out.String())
 }
 
-func getListFolder(myPath string, parent *FolderLikeLeaf) (*FolderLikeLeaf, error) {
+func dirTree(out *bytes.Buffer, myPath string, fileShow bool) error {
+	folder, err := getListFolder(myPath, nil, fileShow)
+	if err != nil {
+		return err
+	}
+	*out = *getByteListFolder(folder, "")
+	return nil
+}
+
+func getListFolder(myPath string, parent *FolderLikeLeaf, fileShow bool) (*FolderLikeLeaf, error) {
 	stat, err := os.Stat(myPath)
 	if err != nil {
 		return nil, err
@@ -70,17 +82,21 @@ func getListFolder(myPath string, parent *FolderLikeLeaf) (*FolderLikeLeaf, erro
 	if err != nil {
 		return nil, err
 	}
-	folder.ChildItem = folders
+	//	folder.ChildItem = folders
 	for _, item := range folders {
 		if item.IsDir() == true {
-			childFolder, err := getListFolder(path.Join(myPath, item.Name()), folder)
+			childFolder, err := getListFolder(path.Join(myPath, item.Name()), folder, fileShow)
 			if err != nil {
 				return folder, err
 			}
 			folder.ChildFolder = append(folder.ChildFolder, childFolder)
+			folder.ChildItem = append(folder.ChildItem, item)
 		}
 		if item.IsDir() != true {
 			folder.ChildFile = append(folder.ChildFile, item.Name())
+			if fileShow {
+				folder.ChildItem = append(folder.ChildItem, item)
+			}
 		}
 	}
 
@@ -151,4 +167,24 @@ func printStringListFolder(listFolder *FolderLikeLeaf, tab string) string {
 		}
 	}
 	return outStr
+}
+
+func getByteListFolder(listFolder *FolderLikeLeaf, tab string) *bytes.Buffer {
+	outBuffer := new(bytes.Buffer)
+	listFolder.sortChildItem()
+	n := len(listFolder.ChildItem)
+	for i, item := range listFolder.ChildItem {
+		if i == n-1 {
+			outBuffer.WriteString(tab + "└───" + item.Name() + "\n")
+			if item.IsDir() == true {
+				outBuffer.Write(getByteListFolder(listFolder.getChildFolder(item.Name()), tab+"	").Bytes())
+			}
+		} else {
+			outBuffer.WriteString(tab + "├───" + item.Name() + "\n")
+			if item.IsDir() == true {
+				outBuffer.Write(getByteListFolder(listFolder.getChildFolder(item.Name()), tab+"│	").Bytes())
+			}
+		}
+	}
+	return outBuffer
 }
