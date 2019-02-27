eae46bac4ee52109bb6fd9bbb55cc36c96ac1e4a
galchenko <galchenko@dep45.niitp>
2019-02-27 10:05:03 +0300

    получил accessToken из header

--- hw4_coverage/client_test.go
+++ hw4_coverage/client_test.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"encoding/xml"
+	"io"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"os"
+	"reflect"
+	"testing"
 )
 
 // код писать тут
@@ -51,7 +56,7 @@ func getUsers(xmlFile string) []User {
 
 	for i, user := range xmlRows.Rows {
 		users[i].Id = user.ID
-		users[i].Name = user.FirstName + user.LastName
+		users[i].Name = user.FirstName + " " + user.LastName
 		users[i].Age = user.Age
 		users[i].About = user.About
 		users[i].Gender = user.Gender
@@ -59,6 +64,41 @@ func getUsers(xmlFile string) []User {
 	return users
 }
 
-func SearchServer(sc *SearchClient, sr *SearchRequest) (*SearchResponse, error) {
-	return &SearchResponse{}, nil
+func SearchServer(w http.ResponseWriter, r *http.Request) {
+	accessToken := "7777" //токен для авторизации
+	at := r.Header.Get("AccessToken")
+	if accessToken != at {
+		io.WriteString(w, `{"status": 401, "err": "not not authorized"}`)
+	}
+}
+
+func TestGetUser(t *testing.T) {
+	users := getUsers("dataset.xml")
+	user := User{0, "Boyd Wolf", 22, `Nulla cillum enim voluptate consequat laborum esse excepteur occaecat commodo nostrud excepteur ut cupidatat. Occaecat minim incididunt ut proident ad sint nostrud ad laborum sint pariatur. Ut nulla commodo dolore officia. Consequat anim eiusmod amet commodo eiusmod deserunt culpa. Ea sit dolore nostrud cillum proident nisi mollit est Lorem pariatur. Lorem aute officia deserunt dolor nisi aliqua consequat nulla nostrud ipsum irure id deserunt dolore. Minim reprehenderit nulla exercitation labore ipsum.
+`, "male"}
+	if !reflect.DeepEqual(users[0], user) {
+		t.Errorf("received %v, expected %v", users[0].About, user.About)
+	}
+}
+
+func TestFindUasers(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	sc := &SearchClient{AccessToken: "7777", URL: ts.URL}
+	searchReq := SearchRequest{
+		Limit:      10,
+		Offset:     0,
+		Query:      "Wolf",
+		OrderField: "Name",
+		OrderBy:    0,
+	}
+	searchRes, err := sc.FindUsers(searchReq)
+	if err != nil {
+		t.Errorf("unexpected error: %#v", err)
+		return
+	}
+	if len(searchRes.Users) < 1 {
+		t.Errorf("unexpected value: %#v", searchRes.Users)
+	}
+
 }
